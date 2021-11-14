c04045f169c651c85e0cc77dd69f6f9a23c6e807
Michael DeSantis <m.desantis628@gmail.com>
2021-11-14 16:44:27 -0500

    it works!

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const body = document.querySelector('body');
 const form = document.forms[0];
 const input = document.querySelector('input');
 const img = document.querySelector('img');
+const div = document.querySelector('div');
 
 let theArray;
+
 const getData = (search) => {
   theArray = [];
   fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=ANIJA3rCnAySYPvxmFd5SOli9wX62Gp5&s=' +
+    'https://api.giphy.com/v1/gifs/search?api_key=ANIJA3rCnAySYPvxmFd5SOli9wX62Gp5&q=' +
       search,
     { mode: 'cors' }
   )
@@ -15,34 +17,30 @@ const getData = (search) => {
       return response.json();
     })
     .then(function (response) {
-      img.src = response.data.images.original.url;
-    });
+      theArray.push(response);
+    })
+    .then(display);
 };
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   getData(`${input.value}`);
+  clear();
 });
 
-function dom(element, attributes = {}, text, parent) {
-  const elem = document.createElement(element);
-  if (attributes) {
-    Object.assign(elem, attributes);
-  }
-  if (text) {
-    elem.innerText = text;
-  }
-  if (parent) {
-    parent.appendChild(elem);
-  }
-  return elem;
-}
+input.addEventListener('submit', clear);
 
 function display() {
   for (let i = 0; i < 49; i++) {
-    let stupidArray = theArray[0];
+    let stupidArray = theArray[0].data;
     let newImg = document.createElement('img');
-    newImg.src = stupidArray[i].url;
-    body.appendChild(newImg);
+    newImg.src = stupidArray[i].images.original.url;
+    div.appendChild(newImg);
+  }
+}
+
+function clear() {
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
   }
 }
